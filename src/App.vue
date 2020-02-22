583a19078270125d084d5db5fb51eedf1d144ef7
tom_glb <goodMorning_glb@atguigu.com>
2020-02-22 23:11:12 +0800

    封装Storage

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,21 @@
 <script>
 // import axios from 'axios'
 // import jsonp from 'jsonp'
+import storage from './storage/index'
 export default {
   name: 'App',
   components: {
   },
-  // data(){
-   
-  // },
+  data(){
+    return {
+
+    }
+  },
   mounted(){
-    
+    // storage.setItem('a', 1);
+    // storage.setItem('user',{a:1});
+    // storage.setItem('abc',{a:1},'user')
+    storage.clear('a');
   }
 }
 </script>
