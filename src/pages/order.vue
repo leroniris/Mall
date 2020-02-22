4d44a374d5ccb7078a4654f7238ccb6f78bc60e4
tom_glb <goodMorning_glb@atguigu.com>
2020-02-22 17:02:58 +0800

    项目目录结构设置

---
+++ src/pages/order.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <nav-head></nav-head>
+        <router-view></router-view>
+        <nav-footer></nav-footer>
+    </div>
+</template>
+<script>
+    export default{
+        name:'nav-header',
+
+    }
+</script>
