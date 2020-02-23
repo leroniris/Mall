76a45c929b8a038e529eb02692c2cedefc2fafe8
tom_glb <goodMorning_glb@atguigu.com>
2020-02-23 23:53:33 +0800

    ServiceBar

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -141,12 +141,13 @@
                 this.axios.get('/products',{
                     params:{
                         categoryId:'100012',
-                        // pageSize:6
+                        pageSize:6
                     }
                 }).then((res=>{
-                    if(res.list.length>6){
-                        this.phoneList = res.list.slice(0,6);
-                    }
+                    // if(res.list.length>6){
+                    //     this.phoneList = res.list.slice(0,6);
+                    // }
+                    this.phoneList = res.list;
                 }))
             },
             goToCart(){
@@ -248,7 +249,7 @@
                             box-shadow:0px 7px 6px 0px rgba(0, 0, 0, 0.11);
                             z-index: 10;
                             transition:all .5s;
-                            // background-color: #ffffff;
+                            background-color: #ffffff;
                             .product{
                                 position: relative;
                                 float:left;
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,15 @@
 <template>
     <div>
-        
+        <service-bar></service-bar>
     </div>
 </template>
 <script>
+    import ServiceBar from './../components/ServiceBar'
     export default{
-        name:'nav-index'
+        name:'index',
+        components:{
+            ServiceBar,
+        }
 
     }
 </script>
